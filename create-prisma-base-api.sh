ce2e57d8a1c5326ec219f798f04e1efc730e3588
Dante <beyondmind9@gmail.com>
2024-02-05 09:15:27 -0300

    mudança nos pagotes gerado pelo create-prisma-base-api

--- create-prisma-base-api.sh
+++ create-prisma-base-api.sh
@@ -1,34 +1,45 @@
-#!/bin/bash
+#!/bin/sh
+
+echo instalando node mais atual
+nvm install --lts
 
 cat << EOF > package.json
 {
   "name": "projeto",
   "version": "1.0.0",
   "description": "",
-  "main": "./dist/index.js",
+  "type": "module",
+  "author": "Prover",
+  "main": "./src/server.js",
   "scripts": {
-    "start": "node ./dist/index.js",
+    "start": "npx prisma migrate deploy && node ./src/server.js",
     "start:dev": "nodemon",
-    "build": "babel src -d dist",
-    "test": "jest",
-    "prisma:push": "prisma db push",
-    "prisma:pull": "prisma db pull",
-    "postinstall":"npx prisma generate && npm run build"
+    "migration": "npx prisma migrate dev",
+    "prisma:push": "echo não use o push!! use o npm run migration",
+    "prisma:pull": "echo não use o pull!! use o npm run migration",
+    "prisma:generate": "prisma generate",
+    "postinstall": "prisma generate",
+    "test:create-prisma-environment": "npm link ./prisma/vitest-environment-prisma",
+    "test:install-prisma-environment": "npm link vitest-environment-prisma",
+    "pretest:e2e": "npm run test:create-prisma-environment && npm run test:install-prisma-environment",
+    "test": "vitest run -c vitest.unit.config.js",
+    "test:e2e": "vitest run -c vitest.e2e.config.js",
+    "test:e2e:watch": "vitest -c vitest.e2e.config.js",
+    "test:coverage": "vitest run --coverage"
   },
   "keywords": [],
-  "author": "",
   "license": "ISC"
 }
 EOF
 
 echo instalando dependencias 
 
-npm i cors express helmet jsonwebtoken
+npm i cors express helmet jsonwebtoken dotenv
 
-npm i -D @babel/cli @babel/core @babel/node @babel/preset-env @babel/types @types/jest eslint eslint-config-standard eslint-plugin-import eslint-plugin-jest eslint-plugin-n eslint-plugin-promise jest nodemon prisma
+npm i -D eslint eslint-config-standard eslint-plugin-import eslint-plugin-n eslint-plugin-promise vitest supertest nodemon prisma
 
 mkdir src
-mkdir -p test/src
+mkdir test
 mkdir .vscode
 
 echo criando arquivos
@@ -37,13 +48,11 @@ node -v > .nvmrc
 
 cat << EOF > .eslintrc.json
 {
-  "plugins": ["jest"],
   "env": {
     "node": true,
-    "es2021": true,
-    "jest/globals": true
+    "es2021": true
   },
-  "extends": ["standard", "plugin:jest/recommended"],
+  "extends": ["standard"],
   "parserOptions": {
     "ecmaVersion": "latest",
     "sourceType": "module"
@@ -52,18 +61,9 @@ cat << EOF > .eslintrc.json
 }
 EOF
 
-cat << EOF > nodemon.json 
-{
-  "execMap": {
-    "js": "babel-node --extensions \".js\" src/index.js"
-  }
-}
-EOF
-
 cat << EOF > .gitignore
 node_modules
 .env
-dist
 EOF
 
 cat << EOF > .vscode/extensions.json 
@@ -84,12 +84,10 @@ cat << EOF > .vscode/settings.json
 {
   "editor.bracketPairColorization.enabled": true,
   "editor.guides.bracketPairs": true,
-  "editor.formatOnSave": true,
   "editor.tabSize": 2,
   "editor.codeActionsOnSave": {
     "source.fixAll.eslint": "explicit"
   },
-  "eslint.validate": ["javascript", "javascriptreact", "typescript", "vue"],
   "[jsonc]": {
     "editor.defaultFormatter": "esbenp.prettier-vscode",
     "editor.formatOnSave": true
@@ -105,21 +103,6 @@ cat << EOF > .vscode/settings.json
 }
 EOF
 
-cat << EOF > .babelrc.json
-{
-  "presets": [
-    [
-      "@babel/preset-env",
-      {
-        "targets": {
-          "node": "current"
-        }
-      }
-    ]
-  ]
-}
-EOF
-
 npx prisma init
 
 cat << EOF > src/prismaClient.js
@@ -130,11 +113,11 @@ const prisma = new PrismaClient()
 export default prisma
 EOF
 
-cat << EOF > src/index.js
+cat << EOF > src/app.js
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
-import whitelist from './whitelist'
+import whitelist from './whitelist.js'
 
 const app = express()
 
@@ -159,8 +142,13 @@ app.get('/', (req, res) => {
   res.send('Olá')
 })
 
+EOF
 
-app.listen(5000, () => console.log('Servidor rodando na porta 5000'))
+cat << EOF > src/server.js
+import { app } from '../app.js'
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log('Servidor rodando na porta 5000'))
 EOF
 
 cat << EOF > src/whitelist.js
@@ -169,7 +157,102 @@ export default [
 ]
 EOF
 
-echo documentações para ler 
+cat << EOF > vitest.config.js
+import { defineConfig } from 'vitest/config'
 
-echo https://jestjs.io/pt-BR/
-echo https://www.prisma.io/docs
+export default defineConfig({
+  test: {}
+})
+
+EOF
+
+cat << EOF > vitest.e2e.config.js
+import { defineConfig, mergeConfig } from 'vitest/config'
+import vitestConfig from './vitest.config.js'
+
+export default mergeConfig(
+  vitestConfig,
+  defineConfig({
+    test: {
+      include: ['**/*.e2e.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      environmentMatchGlobs: [['src/**', 'prisma']]
+    }
+  })
+)
+
+
+EOF
+
+cat << EOF > vitest.unit.config.js
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
+import vitestConfig from './vitest.config.js'
+
+export default mergeConfig(
+  vitestConfig,
+  defineConfig({
+    test: {
+      exclude: [...configDefaults.exclude, '**/*.e2e-{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+    }
+  })
+)
+
+EOF
+
+mkdir  prisma/vitest-environment-prisma
+
+cat << EOF > prisma/vitest-environment-prisma/package.json
+{
+  "name": "vitest-environment-prisma",
+  "version": "1.0.0",
+  "description": "",
+  "main": "prisma-test-environment.js",
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
+}
+
+EOF
+
+cat << EOF > prisma/vitest-environment-prisma/vitest-environment-prisma.js
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { PrismaClient } from '@prisma/client'
+import { execSync } from 'node:child_process'
+
+const prisma = new PrismaClient()
+
+function generateDataBaseUrl (schema) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('please provide a database environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+export default {
+  name: 'prisma',
+  transformMode: 'ssr',
+
+  async setup () {
+    const schema = randomUUID()
+    const databaseUrl = generateDataBaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseUrl
+
+    execSync('npx prisma migrate deploy')
+
+    return {
+      async teardown () {
+        await prisma.\$executeRawUnsafe(\`DROP SCHEMA IF EXISTS "\${schema}" CASCADE\`)
+        await prisma.\$disconnect()
+      }
+    }
+  }
+}
+
+
+EOF
